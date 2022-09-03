4c196c6ccbe5e825574ad6e632f2c65993e2d6a9
risaadodx <wrisaad@gmail.com>
2022-09-03 23:18:53 +0600

    news.js update:category id

--- js/news.js
+++ js/news.js
@@ -8,24 +8,22 @@ const displayCategories = (categories) => {
   const categoriesCointainer = document.getElementById("categories-container");
   categories.forEach((category) => {
     const categoryList = document.createElement("li");
-    //categoryList.classList.add("d-flex");
     categoryList.innerHTML = `
-        <a  href="" onclick = "loadNews('${category.category_id}');">${category.category_name}</a>
+        <a  href="" onclick="loadNews('${category.category_id}');">${category.category_name}</a>
         `;
     categoriesCointainer.appendChild(categoryList);
   });
 };
-const loadNews = async (category) => {
-  const url = `
-  https://openapi.programming-hero.com/api/news/category/01
-  `;
+
+const loadNews = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
   const res = await fetch(url);
   const data = await res.json();
-  displayNews(data.data);
+  console.log(data.data);
 };
-const displayNews = async (news) => {
+const displayNews = async (categories) => {
   const newsContainer = document.getElementById("news_container");
-  for (const post of news) {
+  for (const category of categories) {
     const newsDiv = document.createElement("div");
     newsDiv.classList.add("col");
     newsDiv.innerHTML = `
@@ -33,30 +31,32 @@ const displayNews = async (news) => {
           <div class="row g-0 align-items-center">
             <div class="col-md-3">
               <img
-                src="${post.thumbnail_url}"
+                src="${category.thumbnail_url}"
                 class="img-fluid rounded-start p-3"
                 alt="..."
               />
             </div>
             <div class="col-md-9 pe-4">
               <div class="card-body">
-                <h5 class="card-title fs-3 fw-bold">${post.title}</h5>
+                <h5 class="card-title fs-3 fw-bold">${category.title}</h5>
                 <p class="card-text fw-semibold">
-                  ${post.details.slice(0, 400)}...
+                  ${category.details.slice(0, 400)}...
                 </p>
                 <div class="mt-4 d-flex justify-content-between align-items-center"> 
                   <div class="d-flex ">
                     <div ><img class="author rounded-circle" src=${
-                      post.author.img
+                      category.author.img
                     } alt="" /></div>
                     <div class="ms-3"><h6 class="fw-bold lh-1">${
-                      post.author.name
+                      category.author.name
                     }<h6>
-                        <p class="lh-1">${post.author.published_date}</p></div>
+                        <p class="lh-1">${
+                          category.author.published_date
+                        }</p></div>
                   </div>
                   <div class="d-flex align-items-center">
                     <i class="fa-regular fa-eye fs-4 me-4 "></i>
-                    <h4 class="fw-bold ">${post.total_view}</h4>
+                    <h4 class="fw-bold ">${category.total_view}</h4>
                   </div>
               
                   <div>
