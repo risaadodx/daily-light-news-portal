7c8db06a018b32ba58fe33c00f7f00c0312c27b5
risaadodx <wrisaad@gmail.com>
2022-09-03 21:12:42 +0600

    blog page added

--- js/news.js
+++ js/news.js
@@ -1,31 +1,31 @@
-const loadCategories = () => {
+const loadCategories = async () => {
   const url = `https://openapi.programming-hero.com/api/news/categories`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => displayCategories(data.data.news_category));
+  const res = await fetch(url);
+  const data = await res.json();
+  displayCategories(data.data.news_category);
 };
-const displayCategories = (news_category) => {
+const displayCategories = (categories) => {
   const categoriesCointainer = document.getElementById("categories-container");
-  news_category.forEach((category) => {
+  categories.forEach((category) => {
     const categoryList = document.createElement("li");
     //categoryList.classList.add("d-flex");
     categoryList.innerHTML = `
-        <a href="">${category.category_name}</a>
+        <a  href="" onclick = "loadNews('${category.category_id}');">${category.category_name}</a>
         `;
     categoriesCointainer.appendChild(categoryList);
   });
 };
-const loadNews = (category_id) => {
+const loadNews = async (category) => {
   const url = `
-  https://openapi.programming-hero.com/api/news/category/01 
+  https://openapi.programming-hero.com/api/news/category/01
   `;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => displayNews(data.data));
+  const res = await fetch(url);
+  const data = await res.json();
+  displayNews(data.data);
 };
-const displayNews = (data) => {
+const displayNews = async (news) => {
   const newsContainer = document.getElementById("news_container");
-  data.forEach((data) => {
+  for (const post of news) {
     const newsDiv = document.createElement("div");
     newsDiv.classList.add("col");
     newsDiv.innerHTML = `
@@ -33,43 +33,32 @@ const displayNews = (data) => {
           <div class="row g-0 align-items-center">
             <div class="col-md-3">
               <img
-                src="${data.image_url}"
+                src="${post.thumbnail_url}"
                 class="img-fluid rounded-start p-3"
                 alt="..."
               />
             </div>
             <div class="col-md-9 pe-4">
               <div class="card-body">
-                <h5 class="card-title fs-3 fw-bold">${data.title}</h5>
+                <h5 class="card-title fs-3 fw-bold">${post.title}</h5>
                 <p class="card-text fw-semibold">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
-                  consequatur nulla voluptates beatae! Corporis hic voluptas,
-                  reiciendis voluptatem suscipit fugiat, velit totam animi
-                  ducimus magni eligendi aut quisquam inventore libero optio.
-                  Velit laborum porro reiciendis deleniti dolor. Quae
-                  perspiciatis, dolores quisquam, optio ut veritatis architecto
-                  cupiditate ratione iure aliquid quasi? Velit laborum porro
-                  reiciendis deleniti dolor. Quae perspiciatis, dolores
-                  quisquam, optio ut veritatis architecto cupiditate ratione
-                  iure aliquid quasi?
+                  ${post.details.slice(0, 400)}...
                 </p>
                 <div class="mt-4 d-flex justify-content-between align-items-center"> 
                   <div class="d-flex ">
-                    <div><img src="images/Rectangle 19.png" alt="" /></div>
-                    <div class="ms-3"><h6 class="fw-bold lh-1">Jane Cooper<h6>
-                        <p class="lh-1">Jan 10, 2012</p></div>
+                    <div ><img class="author rounded-circle" src=${
+                      post.author.img
+                    } alt="" /></div>
+                    <div class="ms-3"><h6 class="fw-bold lh-1">${
+                      post.author.name
+                    }<h6>
+                        <p class="lh-1">${post.author.published_date}</p></div>
                   </div>
                   <div class="d-flex align-items-center">
                     <i class="fa-regular fa-eye fs-4 me-4 "></i>
-                    <h4 class="fw-bold ">1.5M</h4>
+                    <h4 class="fw-bold ">${post.total_view}</h4>
                   </div>
-                  <div class=" d-flex justify-content-between w-25">
-                    <i class="fa-regular fa-star-half-stroke fs-4"></i>
-                    <i class="fa-regular fa-star fs-4"></i>
-                    <i class="fa-regular fa-star fs-4"></i>
-                    <i class="fa-regular fa-star fs-4"></i>
-                    <i class="fa-regular fa-star fs-4"></i>
-                  </div>
+              
                   <div>
                     <i class="fa-solid fa-arrow-right fs-4 pe-2 text-primary"></i>
                   </div>
@@ -78,8 +67,8 @@ const displayNews = (data) => {
             </div>
           </div>
         </div>`;
-  });
+    newsContainer.appendChild(newsDiv);
+  }
 };
-
 loadNews();
 loadCategories();
